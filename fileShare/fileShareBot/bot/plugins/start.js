200fc64510cf6f1a80880e8c54b1727b86e0129a
nabilanavab <nabilanavab+telegram@gmail.com>
2023-11-10 18:22:05 +0530

    some minor updates

--- fileShare/fileShareBot/bot/plugins/start.js
+++ fileShare/fileShareBot/bot/plugins/start.js
@@ -1,12 +1,14 @@
 
-const client = require("./bot/bot")
 
-client.addEventHandler(async (update) => {
-    const chatID = Number(update.message.chatID);
-     
-    if (update.message.message.startsWith("/start")) {
-      client.sendMessage(chatID, {
-       message: "Welcome to my Telegram bot!",
-     }); 
-    }
- });
+
+modules.exports = function(client){
+    client.addEventHandler(async (update) => {
+        const chatID = Number(update.message.chatID);
+       
+        if (update.message.message.startsWith("/start")) {
+            client.sendMessage(chatID, {
+                message: "Welcome to my Telegram bot!",
+            });
+       }
+   });
+};
