b9b3feb5a22e05e2372b46fb075e33026a9bdd44
nabilanavab <nabilanavab+telegram@gmail.com>
2023-11-10 16:20:32 +0530

    now loader as a function

--- fileShare/fileShareBot/bot/loader.js
+++ fileShare/fileShareBot/bot/loader.js
@@ -1,38 +1,42 @@
 
 
+const { triggerAsyncId } = require('async_hooks');
 const fs = require('fs');
 const path = require('path');
 
-const root = path.join(__dirname, 'plugins');
+
 
 // List all JavaScript files in the directory and its subdirectories
-const filesToLoad = [];
 
-function walkSync(dir) {
-    fs.readdirSync(dir).forEach((file) => {
-        const filePath = path.join(dir, file);
+function moduleLoader(){
+    const root = path.join(__dirname, 'plugins');
+    const filesToLoad = [];
+
+    fs.readdirSync(root).forEach((file) => {
+        const filePath = path.join(root, file);
         if (fs.statSync(filePath).isDirectory()) {
             walkSync(filePath);
         } else if (file.endsWith('.js')) {
             filesToLoad.push(filePath);
         }
     });
-}
-walkSync(root);
 
-// Load filesToLoad
-for (const modulePath of filesToLoad) {
-    try {
-        const module = require(modulePath);
-    } catch (error) {
-        console.log(`Some Error when importing from ${modulePath}`);
-
-        const indexToRemove = filesToLoad.indexOf(modulePath);
-        if (indexToRemove !== -1) {
-            filesToLoad.splice(indexToRemove, 1);
+    for (const modulePath of filesToLoad){
+        try {
+            const module = require(modulePath);
+        } catch {
+            console.log(`Some Error when importing from ${modulePath}`);
+            const indexToRemove = filesToLoad.indexOf(modulePath);
+            
+            if (indexToRemove !== -1) {
+                filesToLoad.splice(indexToRemove, 1);
+            }
         }
     }
-}
-console.log(`loaded ${filesToLoad.join(', ')}`);
 
-module.exports = { filesToLoad }
+    console.log(`loaded ${filesToLoad.join(', ')}`);
+
+    return true;
+}
+
+module.exports = { moduleLoader }
