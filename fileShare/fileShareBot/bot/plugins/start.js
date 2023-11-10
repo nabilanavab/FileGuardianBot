f1115f4d2a781cae0fae4353623cad23d5a8ef19
nabilanavab <nabilanavab+telegram@gmail.com>
2023-11-11 02:38:18 +0530

    setting filters for messages

--- fileShare/fileShareBot/bot/plugins/start.js
+++ fileShare/fileShareBot/bot/plugins/start.js
@@ -2,9 +2,11 @@
 
 module.exports = function(client){
     client.addEventHandler((update) => {
-        if (update && update.message && update.message.message){
-            console.log("Received new Update");
-            console.log(update.message.message);
+        if (update && update.message && update.message.message && 
+                        update.message.message.startsWith("/start")){
+            
+                console.log(update.message);
+                
         }
     });
 }
