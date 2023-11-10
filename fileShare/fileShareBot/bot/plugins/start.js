940b6a42c23bb2a849320ad5fbd5a7e233ee3fcb
nabilanavab <nabilanavab+telegram@gmail.com>
2023-11-11 03:09:53 +0530

    finally some hope

--- fileShare/fileShareBot/bot/plugins/start.js
+++ fileShare/fileShareBot/bot/plugins/start.js
@@ -1,12 +1,19 @@
 
 
+const { Button } = require("telegram/tl/custom/button");
+
 module.exports = function(client){
     client.addEventHandler((update) => {
         if (update && update.message && update.message.message && 
                         update.message.message.startsWith("/start")){
             
-                console.log(update.message);
-                
+            const markup = client.buildReplyMarkup(Button.inline("Start")); 
+            client.sendMessage(update.message.chatId, {
+                message: "Welcome to my Telegram bot!",
+                buttons: markup,
+                // replyMarkup: 
+            });
+
         }
     });
 }
