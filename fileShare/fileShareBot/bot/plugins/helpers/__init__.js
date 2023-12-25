1e09f87ebc40193d5938ed02918a18958c724dfb
nabilanavab <nabilanavab+telegram@gmail.com>
2023-12-25 11:49:19 +0530

    added some description

---
+++ fileShare/fileShareBot/bot/plugins/helpers/__init__.js
@@ -0,0 +1,14 @@
+
+
+/**
+ * 
+ * @description
+ * Directory Purpose:
+ * - This folder contains  essential functionalities  aimed at  preventing flood bans
+ *                                                         during various operations.
+ * - It includes modules and scripts designed to manage rate limits and ensure smooth
+ *        processing during tasks like forwarding, editing, local file creation, etc.
+ * - These measures are  implemented to avoid  triggering flood detection  mechanisms
+ *                                 that could lead to temporary restrictions or bans.
+ * 
+ */
