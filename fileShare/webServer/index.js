97445efe1f5ff01a34b046f318a858b8ab8aa250
nabilanavab <nabilanavab+telegram@gmail.com>
2023-12-29 12:12:03 +0530

    dynamic result for wabsite based on url

--- fileShare/webServer/index.js
+++ fileShare/webServer/index.js
@@ -1,21 +1,37 @@
-
-
 const express = require('express');
 const path = require('path');
+const fs = require('fs').promises;
 
 const port = process.env.PORT || 3000;
 
-// Serving static files from the 'template' directory [py nostu :)]
-express().use(express.static(path.join(__dirname, 'template')));
+// Serve static files from the 'templates' directory
+express().use(express.static(path.join(__dirname, 'templates')));
 
-// Set up a route for the homepage
-express().get('/', (req, res) => {
-    res.sendFile(
-        path.join(__dirname, 'template', 'index.html')
-    );
-});
+// Middleware to serve dynamic content based on URL path
+express()
+  .use(async (req, res, next) => {
+    try {
+      // Extract path from the URL
+      const urlPath = req.path;
 
-express().listen(port, () => {
-    console.log(`Server Domain Link ${app}`)
+      // Construct the file path based on the URL path
+      const filePath = path.join(__dirname, 'templates', urlPath, 'index.html');
+
+      // Check if the file exists
+      await fs.access(filePath);
+
+      // If the file exists, send it as a response
+      res.sendFile(filePath);
+    } catch (error) {
+      // If the file does not exist, pass the request to the next middleware
+      next();
+    }
+  })
+  .use((req, res) => {
+    // Middleware for handling 404 errors
+    res.status(404).send('Not Found');
+  })
+  .listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
-});
+    console.log(`Public URL: http://localhost:${port}`);
+  });
