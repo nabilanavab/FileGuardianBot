8dc51d67d12cd36ccbe94ec70c2204c306a7d314
✰Naͥbiͣlͫ A Navab✰ <nabilanavab+telegram@gmail.com>
2024-01-01 14:44:59 +0530

    Update index.js

--- FileGuardian/webServer/index.js
+++ FileGuardian/webServer/index.js
@@ -13,69 +13,68 @@
  * 
  */
 
-const file_name = __dirname
-const author = "@nabilanavab"
-
-const express = require('express');
-const path = require('path');
-const fs = require('fs').promises;
-
-const port = process.env.PORT || 3000;
-
-// Create an Express application
+const express = require("express");
 const app = express();
+const port = process.env.PORT || 3001;
 
-// Serve static files from the 'template' directory
-app.use(express.static(path.join(__dirname, 'template')));
 
-/**
- * Middleware to handle dynamic routes for all paths.
- * @param {import('express').Request} req - The Express request object.
- * @param {import('express').Response} res - The Express response object.
- */
-const handleDynamicRoutes = async (req, res) => {
-    try {
-        // Extract path from the URL
-        const urlPath = req.path;
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-        // Construct the file path based on the URL path
-        const filePath = path.join(__dirname, 'templates', urlPath, 'index.html');
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
 
-        // Check if the file exists
-        await fs.access(filePath);
+const html = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hello from Render!</title>
+    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
+    <script>
+      setTimeout(() => {
+        confetti({
+          particleCount: 100,
+          spread: 70,
+          origin: { y: 0.6 },
+          disableForReducedMotion: true
+        });
+      }, 500);
+    </script>
+    <style>
+      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
+      @font-face {
+        font-family: "neo-sans";
+        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
+        font-style: normal;
+        font-weight: 700;
+      }
+      html {
+        font-family: neo-sans;
+        font-weight: 700;
+        font-size: calc(62rem / 16);
+      }
+      body {
+        background: white;
+      }
+      section {
+        border-radius: 1em;
+        padding: 1em;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin-right: -50%;
+        transform: translate(-50%, -50%);
+      }
+    </style>
+  </head>
+  <body>
+    <section>
+      Hello from Render!
+    </section>
+  </body>
+</html>
+`
 
-        // If the file exists, send it as a response
-        res.sendFile(filePath);
-    } catch (error) {
-        // If the file does not exist, redirect to the home page
-        res.redirect('/home');
-    }
-};
-
-// Define dynamic route for all paths
-app.get('*', handleDynamicRoutes);
-
-  /**
-   * Start the Express server.
-   * @returns {Promise<void>} A promise indicating the success or failure of server startup.
-   */
-  const startServer = async () => {
-    try {
-        await app.listen(port);
-        console.log(`Server is running at http://localhost:${port}`);
-    } catch (error) {
-        console.error('Error starting the server:', error.message);
-    }
-};
-
-// Run the server when the script is executed
-if (require.main === module) {
-    startServer();
-}
-
-// Export the Express app for testing or use in other modules
-module.exports = app;
-
+app.get("/", (req, res) => res.type('html').send(html));
 
 /**
  * 
